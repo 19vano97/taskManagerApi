e73fa021cc9de09aa7b1d4f41e5b0656629965e2
19vano97 <vanillin97@gmail.com>
2025-05-04 13:19:15 +0300

    assebly

--- TaskManagerApi/obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
+++ TaskManagerApi/obj/Debug/net9.0/TaskManagerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf637a9c1644045d6ecc816c6d0d5794d0252182")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925733c78de4bef90a3461e7c05db64961eb26d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
